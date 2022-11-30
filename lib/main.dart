628f1bf52653d0553ca3887f8c9fc33aac70fb3e
Rehan Munir <45269512+honeykinng345@users.noreply.github.com>
2022-11-30 15:08:12 +0500

    courie app

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:courier_pro/appBehaviour/my_behaviour.dart';
+import 'package:courier_pro/constant/constant.dart';
+import 'package:courier_pro/pages/splashScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
+    runApp(MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Courier Pro User',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: primaryColor,
+        fontFamily: 'Mukta',
+        textSelectionTheme: TextSelectionThemeData(
+          cursorColor: primaryColor,
+        ),
+        tabBarTheme: TabBarTheme(
+          labelColor: greyColor,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      builder: (context, child) {
+        return ScrollConfiguration(
+          behavior: MyBehavior(),
+          child: child,
+        );
+      },
+      home: SplashScreen(),
+    );
+  }
+}
